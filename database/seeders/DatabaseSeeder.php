74443393929a87d9083f570211c0db9823c895fe
AbiruzzamaMolla <abiruzzaman.molla@gmail.com>
2026-02-26 00:52:20 +0600

    feat: add PostFactory and seed DatabaseSeeder with admin user and sample posts

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,8 +3,10 @@
 namespace Database\Seeders;
 
 use App\Models\Admin;
+use App\Models\Post;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -15,10 +17,14 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        Admin::create([
+        $admin = Admin::create([
             'name' => 'Admin User',
             'email' => 'admin@gmail.com',
-            'password' => \Illuminate\Support\Facades\Hash::make('password'),
+            'password' => Hash::make('password'),
+        ]);
+
+        Post::factory(20)->create([
+            'admin_id' => $admin->id,
         ]);
     }
 }
